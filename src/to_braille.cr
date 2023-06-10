15048249b45d194d8e40007f06b4c4e49c1d02d4
Mate Farkas <mate.farkas@sch.hu>
2023-06-10 13:21:46 +0100

    Add initial implementation

--- src/to_braille.cr
+++ src/to_braille.cr
@@ -1,6 +1,16 @@
 # TODO: Write documentation for `ToBraille`
+require "drawille-cr"
+
 module ToBraille
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  canvas = Drawille::Canvas.new
+
+  STDIN.gets_to_end.each_line.with_index do |line, line_index|
+    line.each_char.with_index do |char, char_index|
+      canvas.set(char_index, line_index) unless char == ' '
+    end
+  end
+
+  puts canvas.render
 end
